ace9fa1468cb51792fcd51b56194e202cb89d4ca
samuelebruni <samuele_bruni@hotmail.com>
2023-12-06 18:06:57 +0100

    add foreign key users table at apartments table, and implementation of barriers that prevent the elimination or modification of apartments belonging to different users

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Apartment extends Model
 {
@@ -17,5 +18,10 @@ class Apartment extends Model
         return $this->belongsToMany(Service::class);
     }
 
-    protected $fillable = ['name','cover_image','description','latitude','longitude','rooms', 'bedrooms', 'beds', 'bathrooms', 'smokers', 'visible'];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $fillable = ['name','cover_image','description','latitude','longitude','rooms', 'bedrooms', 'beds', 'bathrooms', 'smokers', 'visible', 'user_id'];
 }
