9361a5637e3c744f0ff82a85a5b9d036cbd41ce8
samuelebruni <samuele_bruni@hotmail.com>
2023-12-19 22:11:29 +0100

    fix view apartments index.php

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -2,6 +2,78 @@
 
 @section('content')
 
+<section class="container my-5">
+    <div class="d-flex justify-content-between mb-3">
+        <h4 class="text-muted text-uppercase">All Apartments</h4>
+        <a href="{{route('admin.apartments.create')}}" class="btn pink">Add Apartment</a>
+    </div>
+
+    <div class="my-1">
+        @if(session('message'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <strong>{{session('message')}}</strong>
+        </div>
+        @endif
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+        @forelse ($apartments as $apartment)
+        <div class="col-sm-12 col-md-10 col-lg-4 col-xl-3">
+            <div class="card">
+                <a href="{{route('admin.apartments.show', $apartment->slug)}}">
+                    <img class="coverimage" src="{{ str_contains($apartment->cover_image, 'http') ? $apartment->cover_image : asset('storage/' . $apartment->cover_image) }}" class="card-img-top" alt="Apartment Image">
+                </a>
+
+                <div class="card-body">
+                    <h5 class="card-title h_80">{{$apartment->name}}</h5>
+                    @if($apartment->visible)
+                    <p class="text-success mt-3">Apartment is active</p>
+                    @else
+                    <p class="text-danger mt-3">Apartment is not active</p>
+                    @endif
+                    <hr>
+                    <div class="d-flex justify-content-around">
+                        <a href="{{route('admin.apartments.show', $apartment->slug)}}" class="btn pink">View</a>
+                        <a href="{{route('admin.apartments.edit', $apartment->slug)}}" class="btn pink">Edit</a>
+                        <button type="button" class="btn pink" data-bs-toggle="modal" data-bs-target="#modalId-{{$apartment->id}}">
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal Body -->
+        <div class="modal fade" id="modalId-{{$apartment->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitle-{{$apartment->id}}" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalTitle-{{$apartment->id}}">Identificativo appartamento 🏡: {{$apartment->id}}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Attenzione! Se procedi eliminando questo appartamento non potrai più tornare indietro, confermi? 📛
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                        <!-- Delete form -->
+                        <form action="{{route('admin.apartments.destroy', $apartment->slug)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Confirm</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @empty
+        <p>No apartments found.</p>
+        @endforelse
+    </div>
+</section>
+
 <style>
     .pink {
         background-color: #ff385c;
@@ -29,98 +101,10 @@
         width: 100%;
         height: 200px;
     }
-    .h_80{
+
+    .h_80 {
         height: 80px;
     }
 </style>
-
-<section class="container my-5">
-    <div class="d-flex justify-content-between mb-3">
-        <h4 class="text-muted text-uppercase">All Apartments</h4>
-
-features/responsive-site
-            <a href="{{route('admin.apartments.create')}}" class="btn pink">Add Apartment </a>
-        </div>
-
-
-    </div>
-
-    <div class="my-1">
-        @if(session('message'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            <strong>{{session('message')}}</strong>
-        </div>
-        @endif
-    </div>
-
-    <div class="row row-cols-1 row-cols-md-4 g-4">
-
-
-
-
-
-        @forelse ($apartments as $apartment)
-        <div class="col-sm-12 col-md-10 col-lg-4 col-xl-3">
-            <div class="card">
-                <a href="{{route('admin.apartments.show', $apartment->slug)}}">
-                    <img class="coverimage" src="{{ str_contains($apartment->cover_image, 'http') ? $apartment->cover_image : asset('storage/' . $apartment->cover_image) }}" class="card-img-top" alt="Apartment Image">
-                </a>
-
-
-
-
-                <div class="card-body">
-                    <h5 class="card-title h_80">{{$apartment->name}}</h5>
-                    @if($apartment->visible)
-                    <p class="text-success mt-3">Apartment is active</p>
-                    @else
-                    <p class="text-danger mt-3">Apartment is not active</p>
-                    @endif
-                    <hr>
-                    <div class="d-flex justify-content-around">
-                        <a href="{{route('admin.apartments.show', $apartment->slug)}}" class="btn pink">View</a>
-                        <a href="{{route('admin.apartments.edit', $apartment->slug)}}" class="btn pink">Edit</a>
-                        <button type="button" class="btn pink" data-bs-toggle="modal" data-bs-target="#modalId-{{$apartment->id}}">
-                            Delete
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Modal Body -->
-        <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-        <div class="modal fade" id="modalId-{{$apartment->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitle-{{$apartment->id}}" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="modalTitle-{{$apartment->id}}">Identificativo appartamento 🏡: {{$apartment->id}}</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Attenzione! Se procedi eliminando questo appartmaneto non potrai più tornare indietro, confermi? 📛
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-
-                        <!-- Delete form -->
-                        <form action="{{route('admin.apartments.destroy', $apartment->slug)}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Confirm</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        @empty
-        <p>No apartments found.</p>
-        @endforelse
-
-    </div>
-
-</section>
 
 @endsection
