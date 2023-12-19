9ddd0b808065bd9ff0f55aabe73a4456d34f2044
rpsobolewski <rpsobolewski@gmail.com>
2023-12-19 18:17:18 +0100

    style fix

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -24,7 +24,8 @@
         border: 1px solid #ff385c;
         transform: scale(1.05);
     }
-    .coverimage{
+
+    .coverimage {
         width: 100%;
         height: 200px;
     }
@@ -33,11 +34,9 @@
 <section class="container my-5">
     <div class="d-flex justify-content-between mb-3">
         <h4 class="text-muted text-uppercase">All Apartments</h4>
-        <div>
 
-            <a href="{{route('admin.apartments.create')}}" class="btn money me-3">Boost Your Apartment </a>
-            <a href="{{route('admin.apartments.create')}}" class="btn pink">Add Apartment </a>
-        </div>
+
+        <a href="{{route('admin.apartments.create')}}" class="btn pink">Add Apartment </a>
 
     </div>
 
